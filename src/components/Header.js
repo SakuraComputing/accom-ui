bba182157291df08765224e72825b415d13c5cd9
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-25 08:51:30 +0100

    Rationilise project

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,18 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from "../actions/auth";
 
-export const Header = ({ startLogout }) => (
+export const Header = () => (
     <header className="header">
         <div className="content-container">
             <div className="header__content">
                 <Link className="header__title" to="/dashboard">
-                    <h1>Expensify</h1>
+                    <h1>Accomodation UI</h1>
                 </Link>
-                <button className="button button--link" onClick={startLogout}>Logout</button>
             </div>
         </div>
     </header>
 );
 
-const mapDispatchToProps = (dispatch) => ({
-   startLogout: () => dispatch(startLogout())
-});
-
-export default connect(undefined, mapDispatchToProps)(Header);
+export default Header;
 
 
