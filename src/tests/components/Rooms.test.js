236f4302295b156d44632bb07436c374df688a5f
Ewan Bauld <ewan.bauld@gmail.com>
2018-05-29 00:05:56 +0100

    Refactor

--- src/tests/components/Rooms.test.js
+++ src/tests/components/Rooms.test.js
@@ -5,15 +5,6 @@ import data from '../../../data/accommodation_data.json';
 
 
 describe('Rooms Page', () => {
-   it('should render the form correctly', () => {
-       jest.mock('path/to/setting.json', ()=>({
-           settings: 'someSetting'
-       }), { virtual: true });
-
-       const match = { params: jest.fn() };
-      const wrapper = shallow(<Rooms match={match} data={data}/>);
-      expect(wrapper).toMatchSnapshot()
-   });
     it('should have a function called list', () => {
         it('should be a function', () => {
             expect(typeof list).toEqual('function');
