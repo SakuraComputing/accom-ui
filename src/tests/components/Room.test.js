51d84825ef01fb2be124036fcc542b834b717223
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-01 00:14:17 +0100

    Further room coverage

--- src/tests/components/Room.test.js
+++ src/tests/components/Room.test.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Room from '../../components/Room';
+import roomData from '../fixtures/rooms';
 
 describe('Room Page', () => {
+
+    let wrapper;
+
+    beforeEach(() => {
+        const id = 161;
+        wrapper = shallow(<Room id={id}/>);
+    });
     it('should render the form correctly', () => {
-        const wrapper = shallow(<Room/>);
-        expect(wrapper).toMatchSnapshot()
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('should return span tag from getRoomsAvailable', () => {
+        const available = 6;
+        const returnRooms = wrapper.instance().getRoomsAvailable(available);
+        const test = <span className="rooms-available">Rooms available: {available}</span>;
+        expect(returnRooms).toEqual(test);
+
     });
 });
