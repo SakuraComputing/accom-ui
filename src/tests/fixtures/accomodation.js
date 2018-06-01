611784bbc9a91bfc7bbc5e690248707c79d8f42c
Ewan Bauld <ewan.bauld@gmail.com>
2018-06-01 18:00:35 +0100

    Add fixture file

---
+++ src/tests/fixtures/accomodation.js
@@ -0,0 +1,131 @@
+export default {
+    id:65,
+    sort_order:0,
+    name:"Hotel Garni Almhof - In Town - Central Mayrhofen",
+    description:"<p>Alpine bliss! The Almhof sits in a meadow in Mayrhofen, surrounded by magnificent mountain views. Just a short stroll from the centre of the festival and the main ski lifts, you can escape the madness in minutes to chill on your balcony and watch the sun set behind the Penken, or relax in the reading lounge - with free WiFi. Ohh la la.</p> <p>Each room is kitted out with a TV, hairdryer, safe, telephone and balcony overlooking Mayrhofen&rsquo;s snow-capped mountains and the new spa area comes equipped with a Finnish sauna, steam bath and a gym.</p> <p>If you want great value, centrally located traditional Austrian accommodation with clean and simple Tyrolean themed d&eacute;cor, then look no further than the Almhof&hellip;</p>",
+    address_1:"Laubichl 141",
+    address_2:"",
+    address_3:"Mayrhofen",
+    postcode:"A-6290",
+    country:{
+        id:15,
+        name:"Austria"
+    },
+    resort:{
+        id:74,
+        name:"Mayrhofen Central"
+    },
+    location:{
+        id:522,
+        location_long:11.86708005850528,
+        location_lat:47.1726510645752,
+        name:"Hotel Garni Almhof"
+    },
+    images:[{
+        id:8999,
+        filename:"d/1340892898_65_doppelzimmer use.jpg",
+        filename_medium:"d/medium_1340892898_65_doppelzimmer use.jpg",
+        filename_thumb:"d/thumb_1340892898_65_doppelzimmer use.jpg"
+    },
+        {
+            id:9000,
+            filename:"d/1340892898_65_doppelzimmer- oder dreibettzimmer use.jpg",
+            filename_medium:"d/medium_1340892898_65_doppelzimmer- oder dreibettzimmer use.jpg",
+            filename_thumb:"d/thumb_1340892898_65_doppelzimmer- oder dreibettzimmer use.jpg"
+        },
+        {
+            id:9001,
+            filename:"d/1340892898_65_dsc_0028 use.jpg",
+            filename_medium:"d/medium_1340892898_65_dsc_0028 use.jpg",
+            filename_thumb:"d/thumb_1340892898_65_dsc_0028 use.jpg"
+        },
+        {
+            id:9002,
+            filename:"d/1340892898_65_dsc_0072 use.jpg",
+            filename_medium:"d/medium_1340892898_65_dsc_0072 use.jpg",
+            filename_thumb:"d/thumb_1340892898_65_dsc_0072 use.jpg"
+        },
+        {
+            id:9003,
+            filename:"u/1340892898_65_use.jpg",
+            filename_medium:"u/medium_1340892898_65_use.jpg",
+            filename_thumb:"u/thumb_1340892898_65_use.jpg"
+        },
+        {
+            id:9005,
+            filename:"d/1340892898_65_dsc_0056 use.jpg",
+            filename_medium:"d/medium_1340892898_65_dsc_0056 use.jpg",
+            filename_thumb:"d/thumb_1340892898_65_dsc_0056 use.jpg"
+        }
+    ],
+    rating:{
+        id:3,
+        label:"3*"
+    },
+    facilities:[{
+        id:36,
+        label:"Balcony or Terrace"
+    },
+        {
+            id:1,
+            label:"En Suite"
+        },
+        {
+            id:8,
+            label:"Gardens"
+        },
+        {
+            id:28,
+            label:"Restaurant"
+        },
+        {
+            id:10,
+            label:"Safety Deposit Box"
+        },
+        {
+            id:33,
+            label:"Sat TV"
+        },
+        {
+            id:26,
+            label:"Sauna"
+        },
+        {
+            id:34,
+            label:"Telephone"
+        },
+        {
+            id:12,
+            label:"WIFI Access"
+        }
+    ],
+    rooms:[{
+        id:129,
+        sort_order:450,
+        type_id:22,
+        occupancy:2,
+        min_occupancy:2,
+        number_of_core_nights:null,
+        room_type_description:"Austrian Twin/Double",
+        name:"Austrian Twin/Double",
+        upsell:"",
+        description:"<i class=\"fa fa-ticket fa-1\" aria-hidden=\"true\"></i> Festival Ticket Included &nbsp; &nbsp; &nbsp;<i class=\"fa fa-coffee fa-1\" aria-hidden=\"true\"></i> Breakfast Included",
+        website_url:"http://www.almhof-mayrhofen.at",
+        per_night_prices:[]
+    },
+        {
+            id:130,
+            sort_order:0,
+            type_id:4,
+            occupancy:3,
+            min_occupancy:3,
+            number_of_core_nights:null,
+            room_type_description:"Triple",
+            name:"Triple",
+            upsell:"",
+            description:"<i class=\"fa fa-ticket fa-1\" aria-hidden=\"true\"></i> Festival Ticket Included &nbsp; &nbsp; &nbsp;<i class=\"fa fa-coffee fa-1\" aria-hidden=\"true\"></i> Breakfast Included",
+            website_url:"http://www.almhof-mayrhofen.at",
+            per_night_prices:[]
+        }
+    ]
+}
